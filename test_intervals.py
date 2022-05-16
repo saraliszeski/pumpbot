e5898631ae62f6dc6e0e0a5d00c59d906aa934f0
saraliszeski <75336173+saraliszeski@users.noreply.github.com>
2022-05-15 22:31:30 -0400

    Update test_intervals.py

--- test_intervals.py
+++ test_intervals.py
@@ -6,18 +6,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 ctrlPin = 16
 GPIO.setup(ctrlPin, GPIO.OUT)
-try:
-	while(True):
+if __name__ == "__main__":
+	try:
 		GPIO.output(ctrlPin, GPIO.HIGH)
 		time.sleep(1.5)
 		GPIO.output(ctrlPin,GPIO.LOW)
 		time.sleep(0.5)
-		
-
+    		GPIO.cleanup()
 # End program cleanly with keyboard
-except KeyboardInterrupt:
-    print("Quit")
+	except KeyboardInterrupt:
+    		print("Quit")
+		GPIO.cleanup()
+		pass
 
     # Reset GPIO settings
 
-    GPIO.cleanup()
